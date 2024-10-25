866b48a3974b300e744925949f017623a0fcab19
Łukasz Brzózko <lukasz9099@gmail.com>
2024-10-25 18:38:55 +0200

    feat: update meta data

--- src/index.user.js
+++ src/index.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         Userscript starter
-// @namespace    https://github.com/lukasz-brzozko/userscript-starter
-// @version      2024-05-21
-// @description  Creates a new userscript
+// @name         Gitlab Archive Repo Commit Link
+// @namespace    https://github.com/lukasz-brzozko/gitlab-archive-repo-commit-link
+// @version      2024-10-25
+// @description  Redirects to the archived repository if no MR were found
 // @author       Łukasz Brzózko
-// @match        https://github.com/
-// @icon         https://github.githubassets.com/favicons/favicon.png
-// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/userscript-starter/main/dist/index.meta.js
-// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/userscript-starter/main/dist/index.user.js
+// @match        https://gitlab.nd0.pl/orbico/portal/-/commit/*
+// @icon         https://about.gitlab.com/nuxt-images/ico/favicon.ico
+// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/gitlab-archive-repo-commit-link/main/dist/index.meta.js
+// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/gitlab-archive-repo-commit-link/main/dist/index.user.js
 // @grant        none
 // ==/UserScript==
 
