78cf0cb0aedbb97adb54c4b8a7a172a83123ef30
Łukasz Brzózko <lukasz9099@gmail.com>
2024-10-25 20:34:24 +0200

    refac: remove unused code

--- src/index.user.js
+++ src/index.user.js
@@ -2,7 +2,7 @@
 // @name         Gitlab Archive Repo Commit Link
 // @namespace    https://github.com/lukasz-brzozko/gitlab-archive-repo-commit-link
 // @version      2024-10-25
-// @description  Redirects to the archived repository if no MR were found
+// @description  Redirects to the archived repository if no matched MR were found
 // @author       Łukasz Brzózko
 // @match        https://gitlab.nd0.pl/orbico/portal/-/commit/*
 // @icon         https://about.gitlab.com/nuxt-images/ico/favicon.ico
@@ -42,11 +42,7 @@
 
       const setIntervalId = setInterval(() => {
         container = document.querySelector(SELECTORS.container);
-        console.log({ container });
 
-        // const otherInfo = document.querySelectorAll(
-        //   ".info-well>.well-segment+div button"
-        // );
         if (container) {
           clearInterval(setIntervalId);
           window.console.info(
